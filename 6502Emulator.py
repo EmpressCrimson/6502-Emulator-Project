6b568d585d1a09510fcfd4c2d93d5617a21f338f
EmpressCrimson <163685610+EmpressCrimson@users.noreply.github.com>
2026-02-21 20:52:24 +0100

    New Opcodes and working bubble sort algorithm
    Added new opcodes (BCS, TAY, TYA, etc.)
    Still have to fix zeropage opcodes
    Fixed faulty opcode logic such as using offsets to change the program counter ( also turns out I understood some opcodes wrong so changed them too such that they actually worked in the intended way.).

--- 6502Emulator.py
+++ 6502Emulator.py
@@ -16,7 +16,7 @@ class CPU(): #Reasoning is that I don't want to use global variables so I am jus
     def __init__(self):
         self.Running = True
 
-        self.ProgramCounter = 0x0100 #After stack memory
+        self.ProgramCounter = 0x0200 #After stack memory
         
         self.Registers = { #all variables
             "A" : 0,
@@ -47,14 +47,21 @@ class CPU(): #Reasoning is that I don't want to use global variables so I am jus
     def Step(self, RAMObject): #I forgot why these two exist simultaneously
         Value = hex(RAMObject.Read(self.ProgramCounter))
         self.IncrementPC()
-        print("value:" + Value)
+        #print("value:" + Value)
         return Value
     
     def SetStatusReg(self, flags): #OR
         self.StatusRegister = self.StatusRegister | (flags+0x20)
         
     def ClearStatusReg(self, flags): #AND
-        self.StatusRegister = self.StatusRegister & (255-flags)
+        self.StatusRegister = self.StatusRegister & (255-flags+0x20)
+
+    def CheckStatusReg(self, flags):
+        if self.StatusRegister & flags == 0:
+            return False
+        else:
+            return True
+        
     
     def IncDecRegister(self, register, value):
         self.Registers[register] += value
@@ -79,14 +86,14 @@ class CPU(): #Reasoning is that I don't want to use global variables so I am jus
     #region
 
     def NOP(self):
-        print("nop")
+        #print("nop")
         return 0
 
     def BRK(self):
         self.SetRunning(False)
         self.SetStatusReg(0x10)
-        print(self.StatusRegister)
-        print("break")
+        #print(self.StatusRegister)
+        #print("break")
 
     def INX(self):  
         self.IncDecRegister("X", 0x01)
@@ -116,52 +123,131 @@ class CPU(): #Reasoning is that I don't want to use global variables so I am jus
     def LDAImmediate(self):
         #self.Registers["A"] = self.Step(RAM)
         self.Registers["A"] = RAM.Read(self.ProgramCounter)
-        print(self.Registers["A"])
+        #print(self.Registers["A"])
         self.CheckRegZero("A")
+        if self.GetReg("A") < 0:
+            self.SetStatusReg(0x80)
     
     def LDAZeroP(self):
         address = RAM.Read(self.ProgramCounter)
         self.Registers["A"] = RAM.Read(address)
-        print(self.Registers["A"])
+        #print(self.Registers["A"])
         self.CheckRegZero("A")
+        if self.GetReg("A") < 0:
+            self.SetStatusReg(0x80)
 
     def LDXImmediate(self):
         #self.Registers["X"] = self.Step(RAM)
         self.Registers["X"] = RAM.Read(self.ProgramCounter)
-        print(self.Registers["X"])
+        #print(self.Registers["X"])
         self.CheckRegZero("X")
+        if self.GetReg("X") < 0:
+            self.SetStatusReg(0x80)
     
     def LDXZeroP(self):
         address = RAM.Read(self.ProgramCounter)
         self.Registers["X"] = RAM.Read(address)
-        print(self.Registers["X"])
+        #print(self.Registers["X"])
         self.CheckRegZero("X")
+        if self.GetReg("X") < 0:
+            self.SetStatusReg(0x80)
 
     def LDYImmediate(self):
         #self.Registers["Y"] = self.Step(RAM)
         self.Registers["Y"] = RAM.Read(self.ProgramCounter)
-        print(self.Registers["Y"])
+        #print(self.Registers["Y"])
         self.CheckRegZero("Y")
+        if self.GetReg("Y") < 0:
+            self.SetStatusReg(0x80)
     
     def LDYZeroP(self):
         address = RAM.Read(self.ProgramCounter)
         self.Registers["Y"] = RAM.Read(address)
-        print(self.Registers["Y"])
+        #print(self.Registers["Y"])
         self.CheckRegZero("Y")
+        if self.GetReg("Y") < 0:
+            self.SetStatusReg(0x80)
 
     def BNE(self):
         address = RAM.Read(self.ProgramCounter)
-        if (self.StatusRegister&0x02) == 0:
-            self.ProgramCounter = (Stacksize-1) + address-1 #-1 cause while Running increments PC by 1 AFTER setting the PC
-        print("bne")
+        if address & 0x80: #check if signed, if so turn negative
+            address -= 0x100
+        if not (self.StatusRegister & 0x02):
+            self.ProgramCounter += (address)
+        #print("bne")
     
     def BEQ(self):
         address = RAM.Read(self.ProgramCounter)
-        if (self.StatusRegister&0x02) == 1:
-            self.ProgramCounter = (Stacksize-1) + address-1
-        print("beq")
+        if address & 0x80: #check if signed, if so turn negative
+            address -= 0x100
+        if self.StatusRegister & 0x02:
+            self.ProgramCounter += (address)
 
+    def STAZeroP(self):
+        address = RAM.Read(self.ProgramCounter)
+        RAM.Write(address, False, self.Registers["A"]) #fix the zeropage so its actually zeropage
 
+    def STAAbsoluteX(self):
+        address = RAM.Read(self.ProgramCounter+1)
+        address = address<<8 | RAM.Read(self.ProgramCounter)
+        RAM.Write(address+self.Registers["X"], False, self.Registers["A"])
+        #print(address, address+self.Registers["X"])
+
+    def STAAbsolute(self):
+        address = RAM.Read(self.ProgramCounter+1)
+        address = address<<8 | RAM.Read(self.ProgramCounter)
+        RAM.Write(address, False, self.Registers["A"])
+        #print(address, address+self.Registers["X"])
+
+    def LDAAbsoluteX(self):
+        address = RAM.Read(self.ProgramCounter+1)
+        address = address<<8 | RAM.Read(self.ProgramCounter)
+        self.Registers["A"] = RAM.Read(address+self.Registers["X"])
+        self.CheckRegZero("A")
+        if self.GetReg("A") < 0:
+            self.SetStatusReg(0x80)
+        
+    def CMPAbsoluteX(self):
+        address = RAM.Read(self.ProgramCounter+1) #cant believe I missed that a high and low byte exists
+        address = address<<8 | RAM.Read(self.ProgramCounter)
+        Register_A = self.Registers["A"]
+        Compare_Value = RAM.Read((address+self.Registers["X"]) & 0xFFFF) #0xFFFF is for caution I think so I am adding it just in case
+        result = (Register_A - Compare_Value) & 0xFF
+        flags = 0 #turns out I need to clear my flags first
+        self.ClearStatusReg(0x83)
+        if Register_A >= Compare_Value: #turns out this is supposed to compare register A and the compared value not the result itself, smth about carrying over
+            flags += 0x01 #FORGOT THE = SIGN AND I WONDER WHY IT DOESNT WORK
+
+            if Register_A == Compare_Value:
+                flags += 0x02
+
+        if result & 0x80: #I am going to check neg values with this from now on hopefully
+            flags += 0x80
+
+        self.SetStatusReg(flags)
+        print(self.StatusRegister)
+
+    def BCS(self):
+        address = RAM.Read(self.ProgramCounter)
+        if address & 0x80: #check if signed, if so turn negative
+            address -= 0x100
+        if self.StatusRegister & 0x01: #Forgot to erase the "not", forgot why it was there but it was needed that I know
+            self.ProgramCounter += (address)
+
+    def TAY(self):
+        self.Registers["Y"] = self.Registers["A"]
+        self.CheckRegZero("A")
+        if self.GetReg("A") < 0:
+            self.SetStatusReg(0x80)
+
+    def TYA(self):
+        self.Registers["A"] = self.Registers["Y"]
+        self.CheckRegZero("Y")
+        if self.GetReg("Y") < 0:
+            self.SetStatusReg(0x80)
+
+    def RTS(self):
+        self.ProgramCounter = RAM.Read(self.ProgramCounter)
 
     #endregion
 
@@ -204,8 +290,8 @@ def ReadMachineCode():
     data = read_data.replace(b'\r\n',b' ')
     offset = 0
     for byte in data.split(b' '):
-        print(byte)
-        RAM.Write(0x0100+offset, False, int(byte, 2))
+        #print(byte)
+        RAM.Write(0x0200+offset, False, int(byte, 2))
         offset += 0x0001
 
 
@@ -247,6 +333,24 @@ CallTable[0xa4] = (CPU.LDYZeroP, 2, 0x82)
 CallTable[0xd0] = (CPU.BNE, 2, 0x00)
 
 CallTable[0xf0] = (CPU.BEQ, 2, 0x00)
+
+CallTable[0x85] = (CPU.STAZeroP, 2, 0x00)
+
+CallTable[0x8D] = (CPU.STAAbsolute, 3, 0x00)
+
+CallTable[0xBD] = (CPU.LDAAbsoluteX, 3, 0x82)
+
+CallTable[0xDD] = (CPU.CMPAbsoluteX, 3, 0x83)
+
+CallTable[0xB0] = (CPU.BCS, 2, 0x00)
+
+CallTable[0xA8] = (CPU.TAY, 1, 0x82)
+
+CallTable[0x9D] = (CPU.STAAbsoluteX, 3, 0x00)
+
+CallTable[0x98] = (CPU.TYA, 1, 0x82)
+
+CallTable[0x60] = (CPU.RTS, 1, 0x00)
 #endregion
 
 
@@ -281,13 +385,17 @@ CallTable[0xf0] = (CPU.BEQ, 2, 0x00)
 ReadMachineCode()
 # print(RAM.Hexdump())
 print(RAM.Hexdump())
+cycles = 0
 
 while CPU.Running:
     opcode = int(CPU.Step(RAM),16)
-    CPU.IncrementPC
+    #CPU.IncrementPC SINCE WHEN WAS IT LIKE THIS 20/02/2026
     CallTable[opcode][0]() #I spent like an hour just to learn I need to put parantheses here cause without them it just goes NOP and not NOP()
+    print(CallTable[opcode])
     CPU.IncrementPC(CallTable[opcode][1]-1) #Indexing error .d should've been 1 instead of 2
-    print("stepped")
+    cycles += 1
+    #print("a")
+    #print("stepped")
 
 print(f'X: ' + str(CPU.GetReg("X")))
 print(f'Y: ' + str(CPU.GetReg("Y")))
@@ -295,3 +403,6 @@ print(f'A:' + str(CPU.GetReg("A")))
 print(f'StatusRegister ' + (str(bin(CPU.StatusRegister))))
 
 print("program stopped")
+print(RAM.Hexdump())
+print(cycles)
+print(CPU.GetPC())
